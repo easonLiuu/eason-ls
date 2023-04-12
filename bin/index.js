00c5f633392ef975b215c8b5bbdf82c589498636
liujiarui <ljr3289595084@163.com>
2023-04-12 18:52:57 +0800

    feat: node获取文件类型

--- bin/index.js
+++ bin/index.js
@@ -14,7 +14,22 @@ if (!isList) {
     // 遍历当前文件夹下的所有文件, 并排除以.开头的文件或者文件夹
     files.forEach(file => output += file + '            ')
 } else {
+    // bin: rwx r-x r-x
+    // package.json: rw- r-- r--   
+    // r: 访问 w: 编辑 x: 执行
+    // u: 当前登录用户 g: 当前登录用户所在分组 o: 其他用户
+    // bin: 16877 -> 0100 0001 1110 1101
+    // S_IFDIR: 16384 -> 0100 0000 0000 0000
     files.forEach((file, index) => {
+        // 获取文件详细信息
+        const stat = fs.statSync(file)
+        // 获取文件的mode码
+        const mode = stat.mode
+        // 通过"与"判断当前文件是不是目录
+        const isDir = mode & fs.constants.S_IFDIR
+        // 通过"与"判断当前文件是不是文件
+        const isFile = mode & fs.constants.S_IFREG
+        console.log(mode, stat.isDirectory(), isFile > 0, fs.constants.S_IFDIR)
         if (index === files.length - 1) {
             output += file
         } else {
